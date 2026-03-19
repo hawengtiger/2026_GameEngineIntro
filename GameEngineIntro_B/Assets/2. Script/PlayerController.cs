d83aac2ed95b312d0cdaa82ac310d2c4eb08fe03
hawengtiger <hsj63458491@gmail.com>
2026-03-19 15:27:53 +0900

    [Add : 플레이어 기본 움직임
    애니메이션 Idle, Run추가후 PlayerController.cs코드 애니메이터 스크립팅 작업함.

--- GameEngineIntro_B/Assets/2. Script/PlayerController.cs
+++ GameEngineIntro_B/Assets/2. Script/PlayerController.cs
@@ -3,12 +3,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector2 moveInput;
+    /// <summary>
+    /// public
+    /// </summary>
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+
+    /// <summary>
+    /// private
+    /// </summary>
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if(value.isPressed) // 점프 버튼을 누르면
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 
     // Update is called once per frame
@@ -22,6 +47,16 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+
+        if (moveInput.magnitude > 0) // moveinput값이 들어왔을경우. 애니메이션 실행
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
+
         transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
     }
 }
